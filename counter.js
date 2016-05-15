c66167f2f619d3e295ee835145d9ff00f3f018d4
Zhaoyu Wei <weixiaobo88@gmail.com>
2016-05-15 14:25:45 +0800

    zy: move counter function into single file instead in html file

--- counter.js
+++ counter.js
@@ -12,13 +12,31 @@ const counter = (state = 0, action) => {
 const {createStore} = Redux;
 const store = createStore(counter);
 
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+    />,
+    document.getElementById('root'));
 };
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', ()=> {
-  store.dispatch({type :'INCREMENT'});
-});
