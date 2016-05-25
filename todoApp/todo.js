0db57d51dcd7478c2876889f8cedcc8f53fef687
Zhaoyu Wei <weixiaobo88@gmail.com>
2016-05-25 20:25:00 +0800

    zy: create `combineReducers` by scratch

--- todoApp/todo.js
+++ todoApp/todo.js
@@ -43,7 +43,15 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
   }
 };
 
-const {combineReducers} = Redux;
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce((nextState, key) => {
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {})
+  };
+};
+
 const todoApp = combineReducers({
   todos,
   visibilityFilter
