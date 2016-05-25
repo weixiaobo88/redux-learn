de6a0f2551349476fbb30fa94639c8da0290e2f0
Zhaoyu Wei <weixiaobo88@gmail.com>
2016-05-25 21:22:32 +0800

    zy: refactor, extract presentational components

--- todoApp/todo.js
+++ todoApp/todo.js
@@ -71,6 +71,33 @@ const FilterLink = ({filter, currentFilter, children}) => {
   );
 };
 
+const Todo = ({onClick, completed, text}) => (
+  <li
+    onClick={onClick}
+    style={{
+              textDecoration:
+                completed
+                  ? 'line-through'
+                  : 'none'
+          }}
+  >
+    {text}
+  </li>
+);
+
+const TodoList = ({todos, onTodoClick}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={()=> onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case 'SHOW_ALL':
@@ -100,25 +127,10 @@ class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibileTodos.map(todo =>
-            <li key={todo.id}
-                onClick={()=> {
-                  store.dispatch({
-                    type: 'TOGGLE_TODO',
-                    id: todo.id
-                  })
-                }}
-                style={{
-                  textDecoration:
-                    todo.completed
-                      ? 'line-through'
-                      : 'none'
-                }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibileTodos}
+          onTodoClick={id => store.dispatch({type: 'TOGGLE_TODO', id})}
+        />
         <p>
           Show:
           {' '}
